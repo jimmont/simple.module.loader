a42495f92fd5d2503aaa298b2000029c82c268f5
Jim Montgomery <jimmont.com@gmail.com>
2016-11-22 13:49:13 -0800

    adjust protocol again

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
 (function(){
 	var map, config;
 	module.exports = module.require({
-			L: '//cdn.leafletjs.com/leaflet-1.0.0-b1/leaflet.js'
+			L: 'leaflet.js'
 			,options: './things.js'
 			,setCenter: './misc.js'
 	}).then(function(res){
 		config = res.options.setup(res.setCenter, 37.75, -122.4);
-		config.layers = [L.tileLayer('//{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		config.layers = [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			,reuseTiles: true
 			,minZoom: 1
