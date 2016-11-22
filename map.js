dc01f631fe72a809741d74e213b212b4185fd8ec
Jim Montgomery <jimmont.com@gmail.com>
2016-11-22 13:24:58 -0800

    adjust protocol so map loads

--- map.js
+++ map.js
@@ -1,13 +1,13 @@
 (function(){
 	var map, config;
 	module.exports = module.require({
-			L: 'http://cdn.leafletjs.com/leaflet-1.0.0-b1/leaflet.js'
+			L: '//cdn.leafletjs.com/leaflet-1.0.0-b1/leaflet.js'
 			,options: './things.js'
 			,setCenter: './misc.js'
 	}).then(function(res){
 		config = res.options.setup(res.setCenter, 37.75, -122.4);
-		config.layers = [L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		config.layers = [L.tileLayer('//{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="//openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			,reuseTiles: true
 			,minZoom: 1
 			,maxZoom: 18
